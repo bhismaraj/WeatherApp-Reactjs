9c7028d2e32e99db0cc82591bba87cbb772c6739
bhismaraj <105622250+bhismaraj@users.noreply.github.com>
2022-06-14 19:44:41 +0545

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  // const url= 'https://api.openweathermap.org/data/2.5/weather?q=dhangadhi&appid=e1b4a16b8d8736d920d81ff508d83533'
+
+    return (
+        <h1>Hello Ziwin</h1>)
 }
 
-export default App;
+export default App
